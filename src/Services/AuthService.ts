33a277bd1b855abfa318671062871a8aa441e741
Marco Fidel Castro Velasco <marco.castro@correounivalle.edu.co>
2025-10-16 21:08:46 -0500

    add authservice.ts

---
+++ src/Services/AuthService.ts
@@ -0,0 +1,29 @@
+const API_URL = "http://localhost:3000/api/v1"; // cambiar cuando se despliegue en vercel
+
+export async function loginUser(email: string, password: string) {
+  const response = await fetch(`${API_URL}/sessions/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email, password }),
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) throw new Error(data.error || "Error al iniciar sesión");
+
+  return data; // { message, userId }
+}
+
+export async function registerUser(firstName: string, lastName: string, age: number, email: string, password: string) {
+  const response = await fetch(`${API_URL}/users`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ firstName, lastName, age, email, password }),
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) throw new Error(data.error || "Error al registrarse");
+
+  return data;
+}
